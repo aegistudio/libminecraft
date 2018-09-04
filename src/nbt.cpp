8e0972cec60e74bd413b622ee1b27e801db18899
Haoran Luo <1910567646@qq.com>
2018-09-04 22:00:02 +0800

    Replace switch in McDtNbtListRead with template.

--- src/nbt.cpp
+++ src/nbt.cpp
@@ -14,6 +14,9 @@
 // The invalid nbt tag type message.
 static const char* invalidNbtTagType = "Expected invalid nbt tag type.";
 
+// The declaration for the NbtList read method.
+void McIoReadNbtList(McIoInputStream& inputStream, mc::nbtlist& list);
+
 // Implementation for McIoReadNbtCompound.
 void McIoReadNbtCompound(McIoInputStream& inputStream, mc::nbtcompound& compound) {
 	// Just perform reading until an empty item occurs.
@@ -28,6 +31,69 @@ void McIoReadNbtCompound(McIoInputStream& inputStream, mc::nbtcompound& compound
 	}
 }
 
+// To be used in McIoReadNbtList to eliminate switch and #define.
+struct McIoNbtTagListItemRead {
+	typedef std::tuple<McDtNbtList*, int> dstBufferTuple;
+	/**
+	 * @brief The normal read parser for nbt list types.
+	 * @param[out] dstBuffer must be set to the pointer to McDtNbtPayload.
+	 * @param[inout] srcBuffer must be set to the pointer to McIoInputStream.
+	 */
+	template<typename V> static inline void performListRead(
+		McIoInputStream& inputStream, McDtNbtList& list, int listLength) {
+		std::vector<V> listVector;
+		for(size_t i = 0; i < listLength; ++ i) {
+			V data; inputStream >> data;
+			listVector.push_back(data);
+		}
+		mc::nbtlist dataList(listVector);
+		list.swap(dataList);
+	}
+	
+	/**
+	 * @brief The forwarded perform method.
+	 */
+	template<typename V> static inline void perform(
+		bool& valueValid, char* dstBuffer, char* srcBuffer) {
+		assert(srcBuffer != nullptr && dstBuffer != nullptr);
+		
+		dstBufferTuple listInformation 
+			= *reinterpret_cast<dstBufferTuple*>(dstBuffer);
+		
+		performListRead<V>(
+			*reinterpret_cast<McIoInputStream*>(srcBuffer),
+			*std::get<0>(listInformation), std::get<1>(listInformation));
+	}
+};
+
+/// Specialization for McDtNbtList.
+template<> inline void 
+McIoNbtTagListItemRead::performListRead<McDtNbtList>(
+	McIoInputStream& inputStream, McDtNbtList& list, int listLength) {
+	std::vector<mc::nbtlist> listVector;
+	for(size_t i = 0; i < listLength; ++ i) {
+		mc::nbtlist sublist; 
+		McIoReadNbtList(inputStream, sublist);
+		listVector.push_back(std::move(sublist));
+	}
+	mc::nbtlist aggregateList(listVector);
+	list.swap(aggregateList);
+}
+
+/// Specialization for McDtNbtCompound.
+template<> inline void 
+McIoNbtTagListItemRead::performListRead<McDtNbtCompound>(
+	McIoInputStream& inputStream, McDtNbtList& list, int listLength) {
+	std::vector<mc::nbtcompound> listVector;
+	for(size_t i = 0; i < listLength; ++ i) {
+		mc::nbtcompound compound;
+		McIoReadNbtCompound(inputStream, compound);
+		listVector.push_back(std::move(compound));
+	}
+	mc::nbtlist compoundList(listVector);
+	list.swap(compoundList);
+}
+
 // Implementation for McIoReadNbtList.
 void McIoReadNbtList(McIoInputStream& inputStream, mc::nbtlist& list) {
 	mc::s8 listType; inputStream >> listType;
@@ -36,66 +102,23 @@ void McIoReadNbtList(McIoInputStream& inputStream, mc::nbtlist& list) {
 	// Eliminate malformed cases.
 	if(listType < 0 || listType > 12)
 		throw std::runtime_error(invalidNbtTagType);
-	else if(listType == 0 && listLength != 0)
+	else if(listType == 0 && listLength > 0)
 		throw std::runtime_error(invalidNbtTagType);
+	else if(listType == 0) return;
 	
 	// Begin reading of the list.
-	switch(listType) {
-#define McNbtReadListSwitch(dataType)\
-		case mc::nbtinfo.ordinalOf<dataType>() + 1: {\
-			std::vector<dataType> listVector;\
-			for(size_t i = 0; i < listLength; ++ i) {\
-				dataType data; inputStream >> data;\
-				listVector.push_back(data);\
-			}\
-			mc::nbtlist dataList(listVector);\
-			list.swap(dataList);\
-		}; break;
-		
-		McNbtReadListSwitch(mc::s8);
-		McNbtReadListSwitch(mc::s16);
-		McNbtReadListSwitch(mc::s32);
-		McNbtReadListSwitch(mc::s64);
-		McNbtReadListSwitch(mc::f32);
-		McNbtReadListSwitch(mc::f64);
-		McNbtReadListSwitch(mc::jstring);
-		McNbtReadListSwitch(mc::nbtintarray<mc::s8>);
-		McNbtReadListSwitch(mc::nbtintarray<mc::s32>);
-		McNbtReadListSwitch(mc::nbtintarray<mc::s64>);
-		
-		// Special case for nbt list.
-		case mc::nbtinfo.ordinalOf<mc::nbtlist>() + 1: {
-			std::vector<mc::nbtlist> listVector;
-			for(size_t i = 0; i < listLength; ++ i) {
-				mc::nbtlist sublist; 
-				McIoReadNbtList(inputStream, sublist);
-				listVector.push_back(std::move(sublist));
-			}
-			mc::nbtlist aggregateList(listVector);
-			list.swap(aggregateList);
-		}; break;
-		
-		// Special case for nbt compound.
-		case mc::nbtinfo.ordinalOf<mc::nbtcompound>() + 1: {
-			std::vector<mc::nbtcompound> listVector;
-			for(size_t i = 0; i < listLength; ++ i) {
-				mc::nbtcompound compound;
-				McIoReadNbtCompound(inputStream, compound);
-				listVector.push_back(std::move(compound));
-			}
-			mc::nbtlist compoundList(listVector);
-			list.swap(compoundList);
-		}; break;
-	}
+	McIoNbtTagListItemRead::dstBufferTuple parameter(&list, listLength);
+	bool dummy = false;	// In place of valueValid.
+	mc::nbtinfo.byOrdinal<McIoNbtTagListItemRead>(listType - 1, 
+		dummy, (char*)&parameter, (char*)&inputStream);
 }
 
-// To be used by mc::nbtinfo.byOrdinal<V>(), and eliminate duplicate code.
+// To be used in mc::nbt::read() to eliminate switch and #define.
 struct McIoNbtTagItemRead {
 	/**
-	 * @brief The normal read parser for nbt compound types.
+	 * @brief The normal read parser for nbt types.
 	 * @param[out] dstBuffer must be set to the pointer to McDtNbtPayload.
 	 * @param[inout] srcBuffer must be set to the pointer to McIoInputStream.
-	 * @param[out] valueValid unused.
 	 */
 	template<typename V> static inline  void performRead(
 		McIoInputStream& inputStream, McDtNbtPayload& payload) {
@@ -104,10 +127,7 @@ struct McIoNbtTagItemRead {
 	}
 
 	/**
-	 * @brief The normal read parser for nbt compound types.
-	 * @param[out] dstBuffer must be set to the pointer to McDtNbtPayload.
-	 * @param[inout] srcBuffer must be set to the pointer to McIoInputStream.
-	 * @param[out] valueValid unused.
+	 * @brief The forwarded perform method.
 	 */
 	template<typename V> static inline void perform(
 		bool& valueValid, char* dstBuffer, char* srcBuffer) {
